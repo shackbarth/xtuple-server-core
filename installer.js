8a3b7f09b324f77fb3e58c2afd43dda1a7ef1741
tjwebb <travis@traviswebb.com>
2014-03-28 22:34:30 -0400

    small cleanup of installer; TODO change function names

--- installer.js
+++ installer.js
@@ -1,6 +1,15 @@
 (function () {
   'use strict';
 
+  /**
+   * Installer
+   * TODO yea, this whole process should be driven by events. each module binds
+   * to the 'run' event or something in the plan order, and once all preprocessing
+   * is done we just fire that event once and the big wheel starts turning.
+   * Another day
+   */
+  var installer = exports;
+
   var fs = require('fs'),
     path = require('path'),
     format = require('string-format'),
@@ -10,27 +19,14 @@
     clc = require('cli-color'),
     S = require('string'),
     _ = require('underscore'),
-    usage = '',
     install = Commander.version('1.8.0').command('install'),
-    runargs = { };
-
-  var wd = __dirname,
-    installer_dir = path.resolve(wd, '../../'),
-    logo_orange = fs.readFileSync(path.resolve(wd, './x-orange.ascii'), 'ascii').trim(),
-    logo_blue = fs.readFileSync(path.resolve(wd, './x-blue.ascii'), 'ascii').trim(),
-    logo_mono = fs.readFileSync(path.resolve(wd, './x.ascii'), 'ascii').trim(),
-    logo_lines_grey = _.map(logo_mono.split('\n'), function (line) {
-      //var grey = clc.xterm(238);
-      //return grey(line);
-      return line;
-    }),
-    logo_lines_colored = _.map(
+    options = { },
+    logo_orange = fs.readFileSync(path.resolve(__dirname, './x-orange.ascii'), 'ascii').trim(),
+    logo_blue = fs.readFileSync(path.resolve(__dirname, './x-blue.ascii'), 'ascii').trim(),
+    logo_mono = fs.readFileSync(path.resolve(__dirname, './x.ascii'), 'ascii').trim(),
+    logo_lines= _.map(
       _.object(logo_orange.split('\n'), logo_blue.split('\n')),
       function (blue_line, orange_line) {
-        /*
-        var orange = clc.xterm(202), blue = clc.xterm(39);
-        return orange(orange_line) + blue(blue_line);
-        */
         return orange_line + blue_line;
       }
     ),
@@ -40,24 +36,25 @@
         task: task
       });
     },
-    plan = require(path.resolve(wd, 'plan')),
+    plan = require(path.resolve(__dirname, 'plan')),
     current = 1;
-
-  var installer = exports;
 
   _.extend(installer, /** @exports installer */ {
 
     logfile: path.resolve('install.log'),
 
+    /**
+     * Advance progress bar 
+     * @protected
+     */
     log_progress: function (state) {
       console.log(clc.reset);
       _.each(_.range(current), function (i) {
-        console.log(logo_lines_colored[i]);
+        console.log(logo_lines[i]);
       });
 
-      _.each(_.range(current, logo_lines_grey.length), function (i) {
+      _.each(_.tail(logo_lines, current), function (i) {
         console.log();
-        //console.log(logo_lines_grey[i]);
       });
 
       console.log();
@@ -74,6 +71,9 @@
       }
     },
 
+    /**
+     * Log TODO use standard logging lib
+     */
     log: function (payload, stdout) {
       var output = '[{prefix}] {msg}'.format({
         prefix: payload.prefix || 'xtuple',
@@ -85,6 +85,9 @@
       }
     },
 
+    /**
+     * Log an error and kill the process
+     */
     die: function (payload) {
       console.log(clc.reset);
       installer.log({ msg: clc.red.bold(payload.msg), prefix: payload.prefix }, true);
@@ -98,14 +101,15 @@
         stepdata = options;
 
       _.each(plan, function (step) {
-        var taskdata = { };
+        var stepmodule = require(path.resolve('./lib/', step)),
+          taskdata = { };
+          
         stepdata[step.name] = _.extend(taskdata, options[step.name]);
 
         _.each(step.tasks, function (task) {
-          var taskmodule = require(path.resolve(wd, '..', step.name, task));
+          var taskmodule = stepmodule[task];
 
           try {
-            //console.log(stepdata);
             installer.log_progress({ step: step.name, task: task });
             taskdata[task] = _.extend({ }, taskmodule.run(stepdata));
           }
@@ -126,7 +130,7 @@
   _.each(plan, function (step) {
     _.each(step.tasks, function (task) {
       try {
-        var module_path = path.resolve(wd, '..', step.name, task),
+        var module_path = path.resolve(__dirname, '..', step.name, task),
           taskmodule = require(module_path),
           options = _.defaults({ }, taskmodule.options);
         _.each(options, function (option_details, option_name) {
@@ -138,8 +142,8 @@
           install.option(flag, option_details.description);
 
           // set default argument value
-          (runargs[step.name] || (runargs[step.name] = { }));
-          runargs[step.name][option_name] = option_details.value;
+          (options[step.name] || (options[step.name] = { }));
+          options[step.name][option_name] = option_details.value;
         });
       }
       catch (e) {
@@ -167,17 +171,17 @@
       argpath = cleanflag.split('-');
 
     if (!_.isUndefined(install[prop])) {
-      runargs[argpath[0]][argpath[1]] = install[prop];
+      options[argpath[0]][argpath[1]] = install[prop];
     }
   });
 
-  // make sure we aren't missing any required arguments
+  // verify required options
   _.each(plan, function (step) {
     _.each(step.tasks, function (task) {
-      var taskmodule = require(path.resolve(wd, '..', step.name, task));
+      var taskmodule = require(path.resolve(__dirname, '..', step.name, task));
 
       _.each(taskmodule.options, function (option, key) {
-        if (_.isString(option.required) && _.isUndefined(runargs[step.name][key])) {
+        if (_.isString(option.required) && _.isUndefined(options[step.name][key])) {
           installer.log({ msg: 'Missing required argument: ' + key }, true);
           installer.die({ msg: JSON.stringify(option, null, 2) });
         }
@@ -188,17 +192,18 @@
   _.each(plan, function (step) {
     installer.log({ msg: 'Module: {name}'.format(step) }, true);
     installer.log({ msg: '  Tasks: ' + JSON.stringify(step.tasks) }, true);
-    installer.log({ msg: '  Arguments: '+ JSON.stringify(runargs[step.name], null, 2) }, true);
+    installer.log({ msg: '  Arguments: '+ JSON.stringify(options[step.name], null, 2) }, true);
   });
   installer.log({ msg: 'Confirm that the above Installation Plan is Correct'}, true);
 
   prompt.get('Press Enter to Continue', function(err, result) {
-    installer.run(_.extend(runargs, {
+    process.emit('optionsParse', options);
+    installer.run(_.extend(options, {
       logfile: installer.logfile,
       sys: { plan: plan }
     }), true);
-    current = logo_lines_colored.length;
-    installer.log_progress({ step: 'sys', task: 'installer', msg: 'Done!'});
+    current = logo_lines.length;
+    installer.log_progress({ step: 'installer', task: 'installer', msg: 'Done!'});
     process.exit(0);
   });
 })();
