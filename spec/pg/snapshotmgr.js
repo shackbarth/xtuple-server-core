37d08acc17a507026e06c134ea009950aab19ad9
tjwebb <travis@traviswebb.com>
2014-04-15 17:26:59 -0400

    fixed snapshotmgr test

--- spec/pg/snapshotmgr.js
+++ spec/pg/snapshotmgr.js
@@ -9,7 +9,7 @@ var assert = require('chai').assert,
 
 it('should augment the pm2 services config file', function () {
   var pm2config = JSON.parse(fs.readFileSync(options.sys.pm2.configfile).toString());
-  assert.lengthOf(pm2config, 4);
+  assert.lengthOf(pm2config, 3);
 });
 
 describe('cron', function () {
