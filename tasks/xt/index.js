e663c422a9188490d97e8cf23eccf782fbee79a0
tjwebb <travis@traviswebb.com>
2014-04-17 02:33:06 -0400

    cleanup

--- tasks/xt/index.js
+++ tasks/xt/index.js
@@ -5,8 +5,6 @@
   module.exports = {
     clone: require('./clone'),
     database: require('./database'),
-    build_common: require('./build_common'),
-    build_main: require('./build_main'),
     serverconfig: require('./serverconfig'),
     testconfig: require('./testconfig'),
     runtests: require('./runtests')
