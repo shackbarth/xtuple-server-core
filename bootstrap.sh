4e01d10f1f07d6cbd576d12cc496d4b7ff4c960b
tjwebb <travis@traviswebb.com>
2014-04-17 14:18:21 -0400

    fixed extension build bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,25 +14,26 @@ install_debian () {
 
   log "Adding Debian Repositories..."
 
-  sudo apt-get -qq update | tee -a $logfile
-  sudo apt-get -qq autoremove --force-yes
-  sudo apt-get -qq install python-software-properties --force-yes
+  apt-get -qq update | tee -a $logfile
+  apt-get -qq autoremove --force-yes
+  apt-get -qq install python-software-properties --force-yes
 
-  sudo apt-get -qq purge postgresql-${XT_PG_VERSION}* --force-yes
-  sudo apt-get -qq purge nodejs-${XT_NODE_VERSION}* --force-yes
-  sudo apt-get -qq purge npm* --force-yes
+  apt-get -qq purge postgresql-${XT_PG_VERSION}* --force-yes
+  apt-get -qq purge nodejs-${XT_NODE_VERSION}* --force-yes
+  apt-get -qq purge npm* --force-yes
+  apt-get -qq upgrade --force-yes
   
-  sudo wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list > /dev/null
-  sudo add-apt-repository ppa:nginx/stable -y
-  sudo add-apt-repository ppa:chris-lea/node.js-legacy -y
-  sudo add-apt-repository ppa:chris-lea/node.js -y
-  sudo add-apt-repository ppa:git-core/ppa -y
+  wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+  echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list > /dev/null
+  add-apt-repository ppa:nginx/stable -y
+  add-apt-repository ppa:chris-lea/node.js-legacy -y
+  add-apt-repository ppa:chris-lea/node.js -y
+  add-apt-repository ppa:git-core/ppa -y
   
   log "Installing Debian Packages..."
 
-  sudo apt-get -qq update | tee -a $logfile
-  sudo apt-get install curl build-essential libssl-dev openssh-server cups git \
+  apt-get -qq update | tee -a $logfile
+  apt-get -qq install curl build-essential libssl-dev openssh-server cups git \
     nginx-full \
     postgresql-$XT_PG_VERSION postgresql-server-dev-$XT_PG_VERSION postgresql-contrib-$XT_PG_VERSION postgresql-$XT_PG_VERSION-plv8 \
     nodejs=$XT_NODE_VERSION-1chl1~${animal}1 \
@@ -40,7 +41,7 @@ install_debian () {
   | tee -a $logfile
 
   if [[ "$XT_NODE_VERSION" = "0.8.26" ]]; then
-    sudo apt-get -qq install npm --force-yes
+    apt-get -qq install npm --force-yes
   fi
 
   log "All dependencies installed."
@@ -52,20 +53,21 @@ clone_installer () {
   log "npm version: $(npm -v)"
   log "node version: $(node -v)"
 
-  rm -rf xtuple-scripts
   rm -rf /usr/lib/node_modules/xtuple-scripts
   mkdir -p /usr/local/xtuple/src
   cd /usr/local/xtuple/src
+  rm -rf xtuple-scripts
+  npm uninstall -g xtuple-scripts
 
   log "Downloading xTuple Server Tools...\n"
   git clone --recursive https://github.com/xtuple/xtuple-scripts.git
   cd xtuple-scripts 
 
-  npm install -g --production
   npm install
+  npm install -g --production
 
-  log "Running installer self-tests..."
-  npm run-script test-$XT_PG_VERSION
+  #log "Running installer self-tests..."
+  #npm run-script test-$XT_PG_VERSION
 }
 
 log() {
