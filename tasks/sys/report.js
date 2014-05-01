20679b38c604ba46809c040b7d9f5448d467919d
tjwebb <travis@traviswebb.com>
2014-05-01 05:35:09 -0400

    added report task

---
+++ tasks/sys/report.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  var report = exports;
+
+  var lib = require('../../lib'),
+    exec = require('execSync').exec,
+    path = require('path'),
+    _ = require('lodash'),
+    json = require('prettyjson'),
+    clc = require('cli-color');
+
+  _.extend(report, lib.task, /** @exports report */ {
+
+    /** @override */
+    afterInstall: function (options) {
+      options.report = {
+        Credentials: {
+          'xTuple Login': {
+            domain: options.nginx.domain,
+            user: 'admin',
+            password: options.xt.adminpw
+          },
+        }
+      };
+
+      if (options.sys.policy.remotePassword) {
+        options.report['Remote SSH Access'] = {
+          user: 'xtremote',
+          password: options.sys.policy.remotePassword
+        };
+      }
+      if (options.sys.policy.userPassword) {
+        options.report['System User Account'] = {
+          user: options.xt.name,
+          password: options.sys.policy.userPassword
+        };
+      }
+
+      console.log(json.render(options.report));
+      console.log(clc.red.bold('Write this information down now. This report will disappear in 2 minutes.'));
+    }
+
+  });
+})();
+
+
