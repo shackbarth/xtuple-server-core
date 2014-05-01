027abcd74fdf135dd0e4feaad96cd95aefa95371
tjwebb <travis@traviswebb.com>
2014-05-01 01:57:14 -0400

    closes #108

--- tasks/pg/index.js
+++ tasks/pg/index.js
@@ -1,10 +1,4 @@
 /**
  * Configure and manage postgres.
- *
- * XXX I accidentally named this module 'pg'. Why is that a problem?
- * Because there is already a pg module. So if you accidentally require('pg')
- * instead of require('../pg') then your life will become very confusing and
- * sad.
- * TODO change name
  */
 module.exports = require('requireindex')(__dirname);
