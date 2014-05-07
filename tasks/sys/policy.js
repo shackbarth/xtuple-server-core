c84b0b47a87807da5817ff7f213ea2ea4202fcaf
Travis Webb <me@traviswebb.com>
2014-05-07 15:33:19 -0400

    fix ssh config and user roles; resolves #139
    - created sys.ssh module from sys.policy

--- tasks/sys/policy.js
+++ tasks/sys/policy.js
@@ -6,7 +6,7 @@
    */
   var policy = exports;
 
-  var task = require('../../lib/task'),
+  var lib = require('../../lib'),
     fs = require('fs'),
     rimraf = require('rimraf'),
     exec = require('execSync').exec,
@@ -16,7 +16,7 @@
     user_policy_filename = 'XT10-xtuple-user-policy',
     sudoers_d = path.resolve('/etc/sudoers.d');
 
-  _.extend(policy, task, /** @exports policy */ {
+  _.extend(policy, lib.task, /** @exports policy */ {
 
     /** @override */
     beforeInstall: function (options) {
@@ -25,10 +25,11 @@
         'xtadmin',
         'xtremote',
         'root',
-        'admin'
+        'admin',
+        'vagrant'
       ];
       if (_.contains(userBlacklist, options.xt.name)) {
-        throw new Error('Invalid name: '+ options.xt.name);
+        throw new Error('Name for xTuple instance is disallowed: '+ options.xt.name);
       }
     },
 
@@ -53,7 +54,6 @@
     executeTask: function (options) {
       if (exec('id -u {xt.name}'.format(options)).code !== 0) {
         policy.createUsers(options);
-        policy.configureSSH(options);
       }
     },
 
@@ -64,6 +64,7 @@
       exec('rm -f /root/.bash_history');
 
       exec('chmod a-w {xt.configdir}/install-arguments.json'.format(options));
+      exec('chmod a-w {xt.configdir}/install-results.json'.format(options));
     },
 
     /** @override */
@@ -178,35 +179,6 @@
 
       // set user shell to bash
       exec('sudo chsh -s /bin/bash {xt.name}'.format(options));
-    },
-
-    /**
-     * Configure SSH remote access rules.
-     * @private
-     */
-    configureSSH: function  (options) {
-      var src_sshd_conf = fs.readFileSync('/etc/ssh/sshd_config').toString(),
-        rules = {
-          UseDNS: 'no',
-          PermitRootLogin: 'no',
-          // AllowGroups: 'xtadmin xtuser', TODO solve riskiness of installing over ssh
-          LoginGraceTime: '30s',
-          ClientAliveInterval: '60',
-          ClientAliveCountMax: '60',  // keep session alive for one hour
-          X11Forwarding: 'no',
-          PubkeyAuthentication: 'no',
-          HostbasedAuthentication: 'no'
-        },
-        target_sshd_conf = _.reduce(_.keys(rules), function (memo, key) {
-          var regex = new RegExp('^' + key + '.*$', 'gm'),
-            entry = key + ' ' + rules[key],
-            match = regex.exec(memo);
-
-          return match ? memo.replace(match, entry) : memo.concat(entry + '\n');
-        }, src_sshd_conf);
-
-      fs.writeFileSync('/etc/ssh/sshd_config.bak.' + new Date().valueOf(), src_sshd_conf);
-      fs.writeFileSync('/etc/ssh/sshd_config', target_sshd_conf);
     },
 
     /** @override */
