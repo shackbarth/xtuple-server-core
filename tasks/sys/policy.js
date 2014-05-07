5d8e6a78727d65766992679bb3d742d5e70801a6
Travis Webb <me@traviswebb.com>
2014-05-07 05:28:08 -0400

    fixed typo in webmin command installation

--- tasks/sys/policy.js
+++ tasks/sys/policy.js
@@ -37,7 +37,7 @@
       // if customer appears new, that is they've provided no main database,
       // snapshot to restore from, or admin password, generate a admin password
       //console.log('generating adminpw: ', (!options.xt.adminpw && !options.pg.restore && !options.xt.maindb));
-      if (!options.xt.adminpw && !options.pg.restore && !options.xt.maindb) {
+      if (!options.xt.adminpw && !options.xt.maindb) {
         options.xt.adminpw = policy.getPassword();
       }
 
@@ -212,12 +212,13 @@
 
     /** @override */
     uninstall: function (options) {
-      exec('skill -KILL -u {xt.name}'.format(options));
       //exec('skill -KILL -u xtremote'.format(options));
-      exec('deluser {xt.name}'.format(options));
-      exec('deluser xtremote');
-      exec('rm -rf /usr/local/{xt.name}/{xt.version}/xtuple*'.format(options));
-      exec('rm -f '+ path.resolve('/etc/sudoers.d/', user_policy_filename.replace('user', '{xt.name}').format(options)));
+      if (_.isString(options.xt.name)) {
+        exec('skill -KILL -u {xt.name}'.format(options));
+        exec('deluser {xt.name}'.format(options));
+        exec('rm -rf /usr/local/{xt.name}'.format(options));
+        exec('rm -f '+ path.resolve('/etc/sudoers.d/', user_policy_filename.replace('user', '{xt.name}').format(options)));
+      }
     }
   });
 
