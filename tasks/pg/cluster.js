5555ac184abdbc4fa0e746819d90c9035e183780
Travis Webb <me@traviswebb.com>
2014-05-12 10:25:31 -0400

    ci thrash; attempting to remove superuser from admin user

--- tasks/pg/cluster.js
+++ tasks/pg/cluster.js
@@ -58,7 +58,7 @@
           'CREATE ROLE xtrole',
 
           // create 'admin' user (default xtuple client admin)
-          'CREATE ROLE admin WITH LOGIN PASSWORD \'{xt.adminpw}\''.format(options),
+          'CREATE ROLE admin WITH LOGIN PASSWORD \'{xt.adminpw}\' CREATEDB CREATEROLE'.format(options),
 
           // create 'postgres' role for convenience + compatibility
           'CREATE ROLE postgres LOGIN SUPERUSER',
