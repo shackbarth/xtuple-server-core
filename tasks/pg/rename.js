c28137363323ac0ba16fdcebf610c1ae281d3023
Travis Webb <me@traviswebb.com>
2014-05-20 12:12:55 -0400

    initial rename-database implementation

---
+++ tasks/pg/rename.js
@@ -0,0 +1,34 @@
+var lib = require('../../lib'),
+  config = require('./config'),
+  _ = require('lodash');
+
+/**
+ * Rename an existing database.
+ */
+_.extend(exports, lib.task, /** @exports rename-database */ {
+
+  options: {
+    dbname: {
+      required: '<dbname>',
+      description: 'Name of database to operate on'
+    },
+    newname: {
+      required: '<newname>',
+      description: 'New name to assign to the database'
+    }
+  },
+
+  /** @override */
+  beforeTask: function (options) {
+    config.discoverCluster(options);
+  },
+
+  /** @override */
+  executeTask: function (options) {
+    lib.pgCli.psql(options, [
+      'alter database', options.pg.dbname, 'rename to', options.pg.newname
+    ].join(' '));
+  }
+
+});
+
