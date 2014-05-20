5154de7cd8729200eae0ae01b909a3f1fba3de92
Travis Webb <me@traviswebb.com>
2014-05-20 15:25:24 -0400

    drop should update config.js

--- tasks/pg/drop.js
+++ tasks/pg/drop.js
@@ -1,5 +1,6 @@
 var lib = require('../../lib'),
   config = require('./config'),
+  fs = require('fs'),
   exec = require('execSync').exec,
   _ = require('lodash');
 
@@ -23,6 +24,11 @@ _.extend(exports, lib.task, /** @exports fork-database */ {
   /** @override */
   executeTask: function (options) {
     lib.pgCli.dropdb(options, options.xt.name, options.pg.dbname);
+
+    // update config.js
+    var configObject = require(options.xt.configfile);
+    _.pull(configObject.datasource.databases, options.pg.dbname);
+    fs.writeFileSync(options.xt.configfile, lib.xt.build.wrapModule(configObject));
   }
 
 });
