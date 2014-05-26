98cd824f2efd9ae6258a227e542dd6792b5bb3b7
Travis Webb <me@traviswebb.com>
2014-05-25 20:45:57 -0400

    skip host entry only if both version and name match; resolves #227

--- tasks/nginx/etchosts.js
+++ tasks/nginx/etchosts.js
@@ -1,33 +1,34 @@
-(function () {
-  'use strict';
+var lib = require('../../lib'),
+  nginx = require('../nginx'),
+  fs = require('fs'),
+  path = require('path'),
+  os = require('os'),
+  _ = require('lodash'),
+  hosts_template_path = path.resolve(__dirname, 'etc-hosts.template');
 
-  /**
-   * Append entries to the system hosts file if necessary.
-   */
-  var etchosts = exports;
+/**
+ * Append entries to the system hosts file if necessary.
+ */
+_.extend(exports, lib.task, /** @exports etchosts */ {
 
-  var lib = require('../../lib'),
-    nginx = require('../nginx'),
-    fs = require('fs'),
-    path = require('path'),
-    os = require('os'),
-    _ = require('lodash'),
+  /** @override */
+  executeTask: function (options) {
+    var template = fs.readFileSync(hosts_template_path).toString(),
+      customerRegex = 'customer=' + options.xt.name,
+      versionRegex = 'version=' + options.xt.version,
+      hosts = fs.readFileSync(path.resolve('/etc/hosts').toString());
 
-    hosts_template_path = path.resolve(__dirname, 'etc-hosts.template');
 
-  _.extend(etchosts, lib.task, /** @exports etchosts */ {
-
-    /** @override */
-    executeTask: function (options) {
-      var etc_hosts_template = fs.readFileSync(hosts_template_path).toString(),
-        etc_hosts_current = fs.readFileSync(path.resolve('/etc/hosts'));
-
-      if (new RegExp('customer=' + options.xt.name + ',').test(etc_hosts_current)) {
-        // TODO log this event
-      }
-      else {
-        fs.appendFileSync(path.resolve('/etc/hosts'), etc_hosts_template.format(options));
-      }
+    if (new RegExp(customerRegex).test(hosts) && new RegExp(versionRegex).test(hosts)) {
+      // do nothing. the necessary host entry already exists
     }
-  });
-})();
+    else {
+      fs.appendFileSync(path.resolve('/etc/hosts'), template.format(options));
+    }
+  },
+
+  /** @override */
+  uninstall: function (options) {
+
+  }
+});
