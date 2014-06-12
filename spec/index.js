20ffa4a17462874d011d2da32cf9e7d135800c7b
Travis Webb <me@traviswebb.com>
2014-06-12 04:39:46 -0400

    add xtuple as dev dependency

--- spec/index.js
+++ spec/index.js
@@ -9,6 +9,9 @@ var assert = require('chai').assert,
 describe('xTuple Installer', function () {
   global.options = {
       planName: 'install-pilot',
+      local: {
+        workspace: path.resolve(process.cwd(), 'node_modules', 'xtuple')
+      },
       xt: {
         name: 'nonuser',
         version: '4.4.1',
