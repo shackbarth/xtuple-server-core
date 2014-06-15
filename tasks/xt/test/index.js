c1902f9d81ad4b0127c62d17de68f7f2ff12df3c
Travis Webb <me@traviswebb.com>
2014-06-15 16:17:11 -0400

    CREATED xtuple-server-xt-test module

---
+++ tasks/xt/test/index.js
@@ -0,0 +1,28 @@
+var lib = require('xtuple-server-lib'),
+  format = require('string-format'),
+  exec = require('execSync').exec,
+  _ = require('lodash'),
+  path = require('path'),
+  fs = require('fs');
+
+/**
+ * Generate the login_data.js file
+ */
+_.extend(exports, lib.task, /** @exports xtuple-server-xt-test */ {
+
+  /** @override */
+  executeTask: function (options) {
+    exports.writeLoginData(options);
+  },
+
+  writeLoginData: function (options) {
+    fs.writeFileSync(path.resolve(options.xt.usersrc, 'test/lib/login_data.json'), JSON.stringify({
+      data: {
+        webaddress: options.nginx.hostname + ':' + options.nginx.httpsport,
+        username: 'admin',
+        pwd: options.xt.adminpw,
+        org: 'xtuple_demo'
+      }
+    }, null, 2));
+  },
+});
