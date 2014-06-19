d6d3f1cf4a797400eccace84ef63d12b2c8b2c78
Travis Webb <me@traviswebb.com>
2014-06-19 06:42:39 -0400

    updated xt.config with new spec format

---
+++ tasks/xt/config/spec/index.js
@@ -0,0 +1,17 @@
+var assert = require('chai').assert,
+  _ = require('lodash'),
+  fs = require('fs');
+
+exports.afterTask = function (options) {
+
+  it.skip('should be able to read config.js file', function () {
+    assert.match(fs.canRead(options.xt.configfile));
+  });
+  it.skip('should be able to read rand64 file', function () {
+    assert.match(fs.canRead(options.xt.rand64file));
+  });
+  it.skip('should be able to read key256 file', function () {
+    assert.match(fs.canRead(options.xt.key256file));
+  });
+
+});
