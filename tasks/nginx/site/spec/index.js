5912c4f144363c8649143b1f92822bc590207184
Travis Webb <me@traviswebb.com>
2014-07-04 23:37:01 -0400

    removed chai, updated spec and license for nginx-site

--- tasks/nginx/site/spec/index.js
+++ tasks/nginx/site/spec/index.js
@@ -1,18 +1,18 @@
-var assert = require('chai').assert,
+var assert = require('assert'),
   fs = require('fs');
 
 exports.afterExecute = function (options) {
 
   it('should use a valid upstream port', function () {
     var conf = options.nginx.site.string;
-    assert.operator(options.nginx.port, '>=', 8443);
+    assert.ok(options.nginx.port >= 8443, 'port < 8433. not in valid range');
   });
   it.skip('should generate a correct nginx config', function () {
     var conf = fs.readFileSync(options.nginx.siteEnabled).toString();
 
-    assert.match(conf, new RegExp('ssl_certificate_key '+ options.nginx.outkey));
-    assert.match(conf, new RegExp('ssl_certificate '+ options.nginx.outcrt));
-    assert.match(conf, new RegExp('server 127.0.0.1:{nginx.port};'.format(options)));
+    assert.ok(new RegExp('ssl_certificate_key '+ options.nginx.outkey).test(conf));
+    assert.ok(new RegExp('ssl_certificate '+ options.nginx.outcrt).test(conf));
+    assert.ok(new RegExp('server 127.0.0.1:{nginx.port};'.format(options)).test(conf));
   });
 
 };
