63138375df2b22d0d92ecf452d4329d884d62d17
Travis Webb <me@traviswebb.com>
2014-07-06 23:54:46 -0400

    removed a describe tier from the spec planner

--- spec/planner.js
+++ spec/planner.js
@@ -12,65 +12,58 @@ exports.describe = function (spec) {
   var planObject = spec.planObject;
   var plan = planObject.plan;
 
-  describe(planObject.description, function () {
+  describe('('+ planObject.description +')', function () {
 
     before(function () {
       planner.compileOptions(plan, options);
       planner.verifyOptions(plan, options);
     });
 
-    describe('before execute', function () {
-
-      lib.util.eachTask(plan, function (task, phase, taskName) {
-        it(getPackageName(phase.name, taskName) + '#beforeInstall', function () {
-          task.beforeInstall(options);
-        });
+    lib.util.eachTask(plan, function (task, phase, taskName) {
+      it(getPackageName(phase.name, taskName) + '#beforeInstall', function () {
+        task.beforeInstall(options);
       });
+    });
 
-      lib.util.eachTask(plan, function (task, phase, taskName) {
-        var pkgName = getPackageName(phase.name, taskName);
-        var spec = require(pkgName + '/spec');
-        if (_.isFunction(spec.beforeExecute)) {
-          spec.beforeExecute(options);
+    lib.util.eachTask(plan, function (task, phase, taskName) {
+      var pkgName = getPackageName(phase.name, taskName);
+      var spec = require(pkgName + '/spec');
+      if (_.isFunction(spec.beforeExecute)) {
+        spec.beforeExecute(options);
+      }
+    });
+
+    lib.util.eachTask(plan, function (task, phase, taskName) {
+      var pkgName = getPackageName(phase.name, taskName);
+      var spec = require(pkgName + '/spec');
+      var phaseOptions = phase.options || { };
+
+      describe(pkgName, function () {
+        if (phaseOptions.execute === false) return;
+
+        it('#beforeTask, #executeTask, #afterTask', function () {
+          task.beforeTask(options);
+          task.executeTask(options);
+          task.afterTask(options);
+        });
+
+        if (_.isFunction(spec.afterTask)) {
+          spec.afterTask(options);
         }
       });
     });
 
-    describe('execute', function () {
-      lib.util.eachTask(plan, function (task, phase, taskName) {
-        var pkgName = getPackageName(phase.name, taskName);
-        var spec = require(pkgName + '/spec');
-        var phaseOptions = phase.options || { };
+    lib.util.eachTask(plan, function (task, phase, taskName) {
+      var pkgName = getPackageName(phase.name, taskName);
+      var spec = require(pkgName + '/spec');
 
-        describe(pkgName, function () {
-          if (phaseOptions.execute === false) return;
-
-          it('#beforeTask, #executeTask, #afterTask', function () {
-            task.beforeTask(options);
-            task.executeTask(options);
-            task.afterTask(options);
-          });
-
-          if (_.isFunction(spec.afterTask)) {
-            spec.afterTask(options);
-          }
-        });
+      it(pkgName + '#afterInstall', function () {
+        task.afterInstall(options);
+        if (_.isFunction(spec.afterExecute)) {
+          spec.afterExecute(options);
+        }
       });
     });
 
-    describe('after execute', function () {
-
-      lib.util.eachTask(plan, function (task, phase, taskName) {
-        var pkgName = getPackageName(phase.name, taskName);
-        var spec = require(pkgName + '/spec');
-
-        it(pkgName + '#afterInstall', function () {
-          task.afterInstall(options);
-          if (_.isFunction(spec.afterExecute)) {
-            spec.afterExecute(options);
-          }
-        });
-      });
-    });
   });
 };
