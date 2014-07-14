cdb398bb32324c61c4c4e4af2dc3fddbbe59929f
Travis Webb <me@traviswebb.com>
2014-07-14 13:43:27 -0400

    fixed typo in pg-restore validator

--- tasks/pg/restore/index.js
+++ tasks/pg/restore/index.js
@@ -14,7 +14,10 @@ _.extend(exports, lib.task, /** @exports xtuple-server-pg-restore */ {
       optional: '[infile]',
       description: 'Path to the file to be restored',
       validate: function (value, options) {
-        if (_.isEmpty(value) && /restore/.test(options.planeName)) {
+        if (options.planName === 'copy-database') {
+          return value;
+        }
+        if (_.isEmpty(value) && options.planName === 'restore-database') {
           throw new Error('pg-infile must be set');
         }
         if ((options.planName === 'import-users') && ('.sql' !== path.extname(options.pg.infile))) {
