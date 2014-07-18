256a703d8c692b4030e7b42fc07ed1f8c2272f4c
Travis Webb <me@traviswebb.com>
2014-07-18 06:07:07 -0400

    use xt-database in #373

--- tasks/xt/database/index.js
+++ tasks/xt/database/index.js
@@ -20,10 +20,12 @@ _.extend(exports, lib.task, /** @exports xtuple-server-xt-database */ {
         if (_.isEmpty(value) && _.isObject(options.local)) {
           return require(path.resolve(options.local.workspace, 'package')).version;
         }
-        if (_.isNumber(parseInt(value, 16))) {
-          return value;
+        if (/^[0-9A-Fa-f]+$/.test(value)) {
+          options.xt.gitVersion = value.slice(0, 7);
+          return options.xt.gitVersion;
         }
         if (semver.valid(value)) {
+          options.xt.gitVersion = value;
           return value;
         }
 
@@ -112,52 +114,28 @@ _.extend(exports, lib.task, /** @exports xtuple-server-xt-database */ {
   },
 
   /** @override */
-  beforeTask: function (options) {
-    rimraf.sync(path.resolve(options.xt.coredir, 'scripts/lib/build'));
-  },
-
-  /** @override */
   executeTask: function (options) {
-    // build all specified databases
-    _.each(options.xt.database.list, function (db) {
-      try {
-        n(options.n.version);
-        exec(lib.util.getDatabaseBuildCommand(db, options), {
-          stdio: 'pipe',
-          cwd: options.xt.coredir
-        });
-      }
-      catch (e) {
-        log.error('xt-database executeTask', e.message);
-        throw e;
-      }
-      finally {
-        log.verbose('xt-database executeTask', 'finally');
-        n(process.version);
-      }
+    n(options.xt.nodeVersion);
 
-      // install extensions specified by the edition
+    rimraf.sync(path.resolve(options.xt.coredir, 'scripts/lib/build'));
+
+    _.each(options.xt.database.list, function (db) {
+      exports.buildCore(options, db);
       exports.buildExtensions(lib.util.editions[options.xt.edition], db, options);
     });
+
+    n(process.version);
+  },
+  
+  // build all specified databases
+  buildCore: function (options, db) {
+    exec(lib.util.getDatabaseBuildCommand(db, options));
   },
 
+  // install extensions specified by the edition
   buildExtensions: function (extensions, db, options) {
     _.each(extensions, function (ext) {
-      try {
-        n(options.n.version);
-        exec(lib.util.getExtensionBuildCommand(db, options, ext), {
-          stdio: 'pipe',
-          cwd: options.xt.coredir
-        });
-      }
-      catch (e) {
-        log.error('xt-database buildExtensions', e.message);
-        throw e;
-      }
-      finally {
-        log.verbose('xt-database buildExtensions', 'finally');
-        n(process.version);
-      }
+      exec(lib.util.getExtensionBuildCommand(db, options, ext));
     });
   }
 });
